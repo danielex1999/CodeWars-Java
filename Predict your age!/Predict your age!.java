c3af08ce8adc9a7b70a8a402642c81e5b91c0507
Juan Daniel Cavero Tovar <daniel_jul_can@hotmail.com>
2021-10-17 23:38:56 -0500

    Create  Predict your age!.java

---
+++ Predict your age!/Predict your age!.java
@@ -0,0 +1,5 @@
+public class Solution {
+    public static int predictAge(int age1, int age2, int age3, int age4, int age5, int age6, int age7, int age8) {
+        return (int)Math.sqrt(age1*age1+age2*age2+age3*age3+age4*age4+age5*age5+age6*age6+age7*age7+age8*age8)/2;
+    }
+}

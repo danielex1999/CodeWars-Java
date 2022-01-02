27cffe0f853b8a913cfbc848e27391de6e49fe74
Juan Daniel Cavero Tovar <daniel_jul_can@hotmail.com>
2022-01-01 19:41:05 -0500

    Create Numbers in strings.java

---
+++ 7 kyu/Numbers in strings/Numbers in strings.java
@@ -0,0 +1,14 @@
+class Solution{
+    public static int solve(String s){
+        int result=0;
+        String first = s.replaceAll("[\\D]", " ");
+        String[] second = first.split(" ");
+
+        for (String value : second) {
+            if (value.length() > 0 && result < Integer.parseInt(value)) {
+                result = Integer.parseInt(value);
+            }
+        }
+        return result;
+    }
+}

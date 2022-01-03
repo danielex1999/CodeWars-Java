021feb7cbef71f336f6dacc19185478dfb3bf4cc
Juan Daniel Cavero Tovar <daniel_jul_can@hotmail.com>
2022-01-03 08:24:17 -0500

    Create Exes and Ohs.java

---
+++ 7 kyu/Exes and Ohs/Exes and Ohs.java
@@ -0,0 +1,23 @@
+public class XO {
+
+    public static boolean getXO (String str) {
+        int totalO=0;
+        int totalX=0;
+        String words= str.toLowerCase();
+        System.out.println(words);
+
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='o'){
+                totalO++;
+            }
+            if(str.charAt(i)=='x'){
+                totalX++;
+            }
+        }
+        if(totalO==totalX){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}

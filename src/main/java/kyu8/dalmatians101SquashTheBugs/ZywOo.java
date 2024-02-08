c03b49c8fc8fa0104330a182107d68f9eb5b931b
Juan Daniel Cavero Tovar <daniel_jul_can@hotmail.com>
2024-02-08 15:00:00 -0500

    Kyu 8 | 101 Dalmatians - squash the bugs, not the dogs!

--- src/main/java/kyu8/dalmatians101SquashTheBugs/ZywOo.java
+++ src/main/java/kyu8/dalmatians101SquashTheBugs/ZywOo.java
@@ -1,8 +1,14 @@
 package kyu8.dalmatians101SquashTheBugs;
 
 public class ZywOo {
+
     public static String howManyDalmatians(int number) {
+
         String[] dogs = {"Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"};
-        return number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number == 101 ? dogs[3] : dogs[2];
+
+        String respond = number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number == 101 ? dogs[3] : dogs[2];
+
+        return respond;
     }
+
 }
